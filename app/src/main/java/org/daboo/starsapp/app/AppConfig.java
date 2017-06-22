26330a4800f905cd8140e2d6febe4c4fccb3e823
daboome <daboome@gmail.com>
2017-06-22 17:02:40 +0600

    serverAddress property externalized

--- app/src/main/java/org/daboo/starsapp/app/AppConfig.java
+++ app/src/main/java/org/daboo/starsapp/app/AppConfig.java
@@ -1,10 +1,11 @@
 package org.daboo.starsapp.app;
 
+import org.daboo.starsapp.R;
+
 public class AppConfig {
 
     public static final CoreAppEnums.Environment ENVIRONMENT = CoreAppEnums.Environment.DEV;
 
-    public static final String BASE_URL = "http://10.0.3.2:8080";
-//    public static final String BASE_URL = "http://back.daboo.mobi/api";
+    public static final String BASE_URL = GlobalApplication.getInstance().getString(R.string.server_address) + ":8080";
 
 }
